1325f319964c45e567e7a0b694afe83f8fe36713
Debdipbanerjee <debdipbanerjee7@gmail.com>
2022-01-23 02:35:48 -0700

    Added player movement script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //check for keyboard input
+        float input = Input.GetAxisRaw("Horizontal");
+        print(input);
+
+        //Moving the player
+        rb.velocity = new Vector2(input * speed, rb.velocity.y);
+    }
+}
